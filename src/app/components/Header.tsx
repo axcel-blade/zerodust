92f44b873de8c6e28df116819d2e3cff1c4d0419
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-06 23:34:20 +0800

    Updated header component styles and removed SVG files

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -15,10 +15,11 @@ export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className='bg-white'>
+        <header className='bg-red-950'>
         {/*Destop menu*/}
         <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
             <div className="flex lg:flex-1">
+            {/* Logo */}
             <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">ZeroDust</span>
                 <img
@@ -39,16 +40,16 @@ export default function Header() {
             </button>
             </div>
             <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <a href="#" className="text-sm/6 font-semibold text-white hover:text-gray-500">
                 Home
             </a>
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <a href="#" className="text-sm/6 font-semibold text-white hover:text-gray-500">
                 About
             </a>
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <a href="#" className="text-sm/6 font-semibold text-white hover:text-gray-500">
                 Services
             </a>
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <a href="#" className="text-sm/6 font-semibold text-white hover:text-gray-500">
                 Contact
             </a>
             </PopoverGroup>
@@ -58,54 +59,55 @@ export default function Header() {
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
             <div className="fixed inset-0 z-50" />
             <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">ZeroDust</span>
-                <img
-                    alt=""
-                    src="#"
-                    className="h-8 w-auto"
-                />
-                </a>
-                <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-                </button>
-            </div>
-            <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
-                        <a
-                        href="#"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                        >
-                        Home
-                        </a>
-                        <a
-                        href="#"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                        >
-                        About
-                        </a>
-                        <a
-                        href="#"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                        >
-                        Services
-                        </a>
-                        <a
-                        href="#"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                        >
-                        Contact
-                        </a>
+                <div className="flex items-center justify-between">
+                    {/* Logo */}
+                    <a href="#" className="-m-1.5 p-1.5">
+                    <span className="sr-only">ZeroDust</span>
+                    <img
+                        alt=""
+                        src="#"
+                        className="h-8 w-auto"
+                    />
+                    </a>
+                    <button
+                    type="button"
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                    >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon aria-hidden="true" className="size-6" />
+                    </button>
+                </div>
+                <div className="mt-6 flow-root">
+                    <div className="-my-6 divide-y divide-gray-500/10">
+                        <div className="space-y-2 py-6">
+                            <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                            >
+                            Home
+                            </a>
+                            <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                            >
+                            About
+                            </a>
+                            <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                            >
+                            Services
+                            </a>
+                            <a
+                            href="#"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                            >
+                            Contact
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
             </DialogPanel>
         </Dialog>
         </header>
